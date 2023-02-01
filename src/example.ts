c26b211330a47db63b2dd5c646a0c788ce01d5fc
Lyallm1 <81273053+Lyallm1@users.noreply.github.com>
2023-02-01 01:53:33 +0000

    modify

--- src/example.ts
+++ src/example.ts
@@ -1,13 +1,31 @@
-import { DecisionTree, RandomForest } from "./woodlands.js"
+// import fs from "fs"
 
-const trainingData = [
-    { a: 0, b: 0, output: 0 }, { a: 0, b: 1, output: 1 }, { a: 1, b: 0, output: 1 }, { a: 1, b: 1, output: 0 }
-], testingData = [{ a: 0, b: 0, output: 0 }, { a: 0, b: 1, output: 1 }], dt = new DecisionTree(trainingData, 'output', ['a', 'b'])
-console.log(`Single training accuracy: ${dt.evaluate(trainingData)}`)
-console.log(`Single training accuracy: ${dt.evaluate(testingData)}`)
-console.log(dt.predict({ a: 0, b: 1 }))
-const rf = new RandomForest(trainingData, 'output', ['a', 'b'], { numTrees: 100, percentData: 1, percentFeatures: 1 })
-console.log('Evaluating forest...')
-console.log(JSON.stringify(rf.evaluate(testingData), null, '\t'))
-console.log(rf.predictProbability({ a: 0, b: 1 }))
-console.log(rf.predictClass({ a: 0, b: 1 }))
+// class DecisionNode {
+//     constructor(public left: DecisionNode, public right: DecisionNode, public decisionFunction: (feature) => boolean, public classLabel = null) {}
+
+//     decide(feature) {
+//         return this.classLabel ? this.classLabel : this.decisionFunction(feature) ? this.left.decide(feature) : this.right.decide(feature)
+//     }
+// }
+
+// function loadCSV(dataFilePath: string, classIndex = -1): [number[][], number[][]] | [number[][], number[]] | number[][] {
+//     const out: number[][] = []
+//     for (const r in fs.createReadStream(dataFilePath)) if (r) for (const i of r.split(',')) out.push([Number(i)])
+//     return classIndex == -1 ? [out.slice().slice(0, classIndex), out.slice().slice(classIndex)] : classIndex == 0 ? [out.slice().slice(1), out.slice()[classIndex]] : out
+// }
+// function giniImpurity(classVector: any[]) {
+    
+// }
+
+class Counter {
+    constructor(arr: any[]) {
+        arr.forEach(val => this[val] = (this[val] || 0) + 1)
+    }
+
+    *[Symbol.iterator]() {
+        for (const key in this)yield [key, this[key]]
+    }
+}
+
+const lst = ['a', 'b', 'c', 'b', 'a', 'b', 'c', 'a', 'a', 'a']
+for (const [key, val] of new Counter(lst)) console.log({ key, val })
