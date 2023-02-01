4c0fe51a037426d61cc59de379e76ecc71374b7b
Lyallm1 <81273053+Lyallm1@users.noreply.github.com>
2023-02-01 03:10:11 +0000

    modify

---
+++ src/example.ts
@@ -0,0 +1,18 @@
+// const trainingData: [string, number, string][] = [['Green', 3, 'Apple'], ['Yellow', 3, 'Apple'], ['Red', 1, 'Grape'], ['Red', 1, 'Grape'], ['Yellow', 3, 'Lemon']],
+// header = ['colour', 'diameter', 'label']
+
+// function uniqueVals(rows: [string, number, string][], col: number) {
+//     const unique = new Set<string | number>()
+//     for (const row of rows) unique.add(row[col])
+//     return unique
+// }
+// function classCounts(rows: [string, number, string][]) {
+//     const counts = {}
+//     for (const row of rows) {
+//         const label = row.slice()
+//     }
+//     return counts
+// }
+
+const lst = ['a', 'b', 'c', 'd', 'e']
+console.log(lst[-1])
