0a9592bdc96864922a398e9a88498e6ff1aaf04f
Lyallm1 <81273053+Lyallm1@users.noreply.github.com>
2023-02-01 02:26:01 +0000

    modify

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-class Counter {
-    constructor(arr: any[]) {
-        arr.forEach(val => this[val] = (this[val] || 0) + 1)
-    }
-
-    *[Symbol.iterator]() {
-        for (const key in this)yield [key, this[key]]
-    }
-}
