6edb73ba4740a256dcb9b94bdf2cfd4275621f00
Lyallm1 <81273053+Lyallm1@users.noreply.github.com>
2023-02-02 02:59:24 +0000

    modify

--- src/example.ts
+++ src/example.ts
@@ -1,13 +1,7 @@
-import { DecisionTree, RandomForest } from "./woodlands.js"
+import { evaluateAlgorithm, loadCSV, randomForest } from './woodlands.js'
 
-const trainingData = [
-    { a: 0, b: 0, output: 0 }, { a: 0, b: 1, output: 1 }, { a: 1, b: 0, output: 1 }, { a: 1, b: 1, output: 0 }
-], testingData = [{ a: 0, b: 0, output: 0 }, { a: 0, b: 1, output: 1 }], dt = new DecisionTree(trainingData, 'output', ['a', 'b'])
-console.log(`Single training accuracy: ${dt.evaluate(trainingData)}`)
-console.log(`Single test accuracy: ${dt.evaluate(testingData)}`)
-console.log(dt.predict({ a: 0, b: 1 }))
-const rf = new RandomForest(trainingData, 'output', ['a', 'b'], { numTrees: 100, percentData: 1, percentFeatures: 1 })
-console.log('Evaluating forest...')
-console.log(JSON.stringify(rf.evaluate(testingData), null, '\t'))
-console.log(rf.predictProbability({ a: 0, b: 1 }))
-console.log(rf.predictClass({ a: 0, b: 1 }))
+for (const nTrees of [1, 10, 50, 100]) {
+    const scores = evaluateAlgorithm(loadCSV('data/small_result.csv'), randomForest, 5, 10, 1, 1, nTrees, 27)
+    console.log(`Trees: ${nTrees}, Scores: ${scores}`)
+    console.log(`Mean accuracy: ${scores.reduce((a, b) => a + b, 0) / scores.length}`)
+}
--- src/woodlands.ts
+++ src/woodlands.ts
@@ -1,150 +1,111 @@
 import _ from 'lodash'
+import fs from 'fs'
 
-function randomTag() {
-    return `_r${Math.round(Math.random() * 1000000)}`
+export function loadCSV(filename: string) {
+    const dataset = []
+    for (const row in fs.createReadStream(filename)) {
+        if (!row) continue
+        dataset.push(row)
+    }
+    return dataset
 }
-function mostCommon(l: any[]) {
-    return _.sortBy(l, a => _.filter(l, b => b == a)).reverse()[0]
+function crossValidationSplit(dataset: any[][], nFolds: number) {
+    const datasetSplit = [], datasetCopy = new Array(dataset)
+    for (let i = 0; i < nFolds; i++) {
+        const fold = []
+        while (fold.length < dataset.length / nFolds) fold.push(datasetCopy[Number(_.range(datasetCopy.length))])
+        datasetSplit.push(fold)
+    }
+    return datasetSplit
 }
-function entropy(vals: any[]) {
-    return _.uniq(vals).map(x => _.filter(vals, val => val == x).length / vals.length).map(p => -p * Math.log2(p)).reduce((a, b) => a + b, 0)
+function accuracyMetric(actual: any[][], predicted: any[][]) {
+    let correct = 0
+    for (const [i, value] of actual.entries()) if (value == predicted[i]) correct++
+    return correct * 100 / actual.length
 }
-function gain(_s: any[], target: string, feature: string) {
-    return entropy(_.map(_s, target)) - _.uniq(_.map(_s, feature)).map(n => {
-        const subset = _s.filter(x => x[feature] == n)
-        return subset.length * entropy(_.map(subset, target)) / _.size(_s)
-    }).reduce((a, b) => a + b, 0)
+function testSplit(index: number, value: number, dataset: any[][]): [any[][], any[][]] {
+    const left: any[][] = [], right: any[][] = []
+    for (const row of dataset) row[index] < value ? left.push(row) : right.push(row)
+    return [left, right]
 }
-function createTree(_s: any[], target: string, features: string[]): { type: 'result' | 'feature', val?: any, vals?: any[], name: string, alias: string } {
-    const targets = _.uniq(_.map(_s, target)), topTarget = mostCommon(targets), bestFeature = _.maxBy(features, e => gain(_s, target, e)), possibleValues = _.uniq(_.map(_s, bestFeature))
-    if (targets.length == 1) return { type: 'result', val: targets[0], name: targets[0], alias: targets[0] + randomTag() }
-    if (features.length == 0 || possibleValues.length == 1) return { type: 'result', val: topTarget, name: topTarget, alias: topTarget + randomTag() }
-    return { name: bestFeature, alias: bestFeature + randomTag(), type: 'feature', vals: _.map(possibleValues, v => ({
-        name: v, alias: v + randomTag(), type: 'feature_value', child: createTree(_s.filter(x => x[bestFeature] == v), target, _.without(features, bestFeature))
-    })) }
+function giniIndex(groups: any[][][], classValues: any[]) {
+    let gini = 0
+    for (const classValue of classValues) for (const group of groups) {
+        const size = group.length
+        if (size == 0) continue
+        let prop = 0
+        for (const row of group) prop += [row[-1]].indexOf(classValue) / size
+        gini += prop * (1 - prop)
+    }
+    return gini
 }
-
-export class DecisionTree {
-    data: any[]
-    model = createTree([], '', [])
-
-    constructor(_s: any[], public target: string, public features: string[]) {
-        this.data = _s
-        this.model = createTree(_s, target, features)
+function getSplit(dataset: any[][], nFeatures: number) {
+    let bIndex = Infinity, bValue = Infinity, bScore = Infinity, bGroups = null
+    const features = []
+    while (features.length < nFeatures) {
+        const index = Number(_.range(dataset[0].length - 1))
+        if (!(index in features)) features.push(index)
     }
-
-    predict(sample: { a: number, b: number, output?: number }) {
-        let root = this.model
-        while (root.type != 'result') {
-            const childNode = _.find(root.vals, x => x.name == sample[root.name])
-            root = childNode ? childNode.child : root.vals[0].child
-        }
-        return root.val
+    for (const index of features) for (const row of dataset) {
+        const groups = testSplit(index, row[index], dataset), gini = giniIndex(groups, new Array(new Set(row[-1])))
+        if (gini < bScore) [bIndex, bValue, bScore, bGroups] = [index, row[index], gini, groups]
     }
-
-    evaluate(samples: { a: number, b: number, output: number }[]) {
-        let total = 0, correct = 0
-        _.each(samples, s => {
-            total++
-            if (this.predict(s) == s[this.target]) correct++
-        })
-        return correct / total
-    }
-
-    featureImportance() {
-        const r = {}
-        for (const feature of this.features) r[feature] = gain(this.data, this.target, feature)
-        return r
-    }
-
-    toJSON() {
-        return this.model
-    }
+    return new Map([['index', bIndex], ['value', bValue], ['groups', bGroups]])
 }
-export class RandomForest {
-    numTrees: number
-    percentData: number
-    percentFeatures: number
-    verbose: boolean
-    data: { a: number, b: number, output: number }[]
-    features: string[]
-    trees: DecisionTree[] = []
-
-    constructor(_s: { a: number, b: number, output: number }[], public target: string, features: string[], opts: { numTrees: number, percentData: number, percentFeatures: number, verbose?: boolean }) {
-        this.numTrees = opts.numTrees || 100
-        this.percentData = opts.percentData || 0.2
-        this.percentFeatures = opts.percentFeatures || 0.7
-        this.verbose = opts.verbose || false
-        this.data = _s
-        this.features = features.slice(0)
-        for (let i = 0; i < this.numTrees; i++) {
-            let d = _s.slice(0)
-            d = _.slice(_.shuffle(d), 0, d.length * this.percentData)
-            const f = _.slice(_.shuffle(features.slice(0)), 0, Math.round(features.length * this.percentFeatures))
-            if (this.verbose) {
-                console.log(`Tree ${i} : ${d.length} data / ${f.length} features`)
-                console.log(JSON.stringify(f.sort()))
-            }
-            this.trees.push(new DecisionTree(d, target, f))
-        }
+function toTerminal(group: any[][]) {
+    const outcomes = []
+    for (const row of group) outcomes.push(row[-1])
+    return _.maxBy(outcomes, outcomes.indexOf)
+}
+function split(node: Map<string, any>, maxDepth: number, minSize: number, nFeatures: number, depth: number) {
+    const [left, right] = node['groups'] as [any[][], any[][]]
+    delete node['groups']
+    if (!(left && right)) {
+        node['left'] = node['right'] = toTerminal(left.concat(right))
+        return
     }
-
-    predictClass(sample: { a: number, b: number, output?: number }) {
-        return this.predict(sample, 'class')
+    if (depth >= maxDepth) {
+        [node['left'], node['right']] = [toTerminal(left), toTerminal(right)]
+        return
     }
-
-    predictProbability(sample: { a: number, b: number, output?: number }) {
-        return this.predict(sample, 'probability')
-    }
-
-    predict(sample: { a: number, b: number, output?: number }, type: 'class' | 'probability') {
-        type = type || 'class'
-        const results = []
-        _.each(this.trees, dt => results.push(dt.predict(sample)))
-        if (type == 'class') return mostCommon(results)
-        else {
-            const counts: {[key: number]: number} = {}
-            for (const num of results) counts[num] = counts[num] ? counts[num] + 1 : 1
-            _.each(counts, e => e /= results.length)
-            return counts
+    node['left'] = left.length <= minSize ? toTerminal(left) : getSplit(left, nFeatures)
+    split(node['left'], maxDepth, minSize, nFeatures, depth + 1)
+    node['right'] = right.length <= minSize ? toTerminal(right) : getSplit(right, nFeatures)
+    split(node['right'], maxDepth, minSize, nFeatures, depth + 1)
+}
+function buildTree(train: any[][], maxDepth: number, minSize: number, nFeatures: number) {
+    const root = getSplit(train, nFeatures)
+    split(root, maxDepth, minSize, nFeatures, 1)
+    return root
+}
+function predict(node: Map<string, any>, row: any[]) {
+    return row[node['index']] < node['value'] ? (node['left'] instanceof Map ? predict(node['left'], row) : node['left']) : (node['right'] instanceof Map ? predict(node['right'], row) : node['right'])
+}
+function baggingPredict(trees: Map<string, any>[], row: any[]) {
+    const predictions = []
+    for (const tree of trees) predictions.push(predict(tree, row))
+    return _.maxBy(predictions, predictions.indexOf)
+}
+function subsample(dataset: any[][], ratio: number) {
+    const sample = []
+    while (sample.length < Math.round(dataset.length * ratio)) sample.push(dataset[Number(_.range(dataset.length))])
+    return sample
+}
+export function randomForest(train: any[][], test: any[][], maxDepth: number, minSize: number, sampleSize: number, nTrees: number, nFeatures: number) {
+    const forest = []
+    for (let i = 0; i < nTrees; i++) for (const row of test) forest.push(baggingPredict([buildTree(subsample(train, sampleSize), maxDepth, minSize, nFeatures)], row))
+    return forest
+}
+export function evaluateAlgorithm(dataset: any[][], algorithm: (trainSet: any[], testSet: any[], ...args: any[]) => any, nFolds: number, ...args: any[]) {
+    const folds = crossValidationSplit(dataset, nFolds), scores = []
+    for (const fold of folds) {
+        const trainSet = new Array(folds), testSet = []
+        for (const row of fold) {
+            const rowCopy = new Array(row)
+            testSet.push(rowCopy)
+            rowCopy[-1] = null
+            scores.push(accuracyMetric([row[-1]], algorithm(trainSet, testSet, ...args)))
         }
     }
-
-    evaluate(samples: { a: number, b: number, output: number }[]) {
-        const report = { size: 0, correct: 0, incorrect: 0, accuracy: 0, precision: 0, recall: 0, fscore: 0, class: {}, featureImportance: null }
-        _.each(samples, s => {
-            report.size++
-            const pred = this.predictClass(s), actual = s[this.target]
-            report.class[pred] = report.class[pred] || { size: 0, predicted: 0, predictedCorrect: 0 }
-            report.class[pred]['predicted']++
-            report.class[actual] = report.class[actual] || { size: 0, predicted: 0, predictedCorrect: 0 }
-            report.class[pred]['size']++
-            if (pred == actual) {
-                report.correct++
-                report.class[pred]['predictedCorrect']++
-            } else report.incorrect++
-        })
-        let classLength = 0
-        _.each(report.class, d => {
-            (d['precision'] as number) = d['predictedCorrect'] / d['predicted'];
-            (d['recall'] as number) = d['predictedCorrect'] / d['size'];
-            (d['fscore'] as number) = 2 / (1 / d['precision'] + 1 / d['recall'])
-            report.precision += d['precision']
-            report.recall += d['recall']
-            report.fscore += d['fscore']
-            classLength++
-        })
-        report.accuracy = report.correct / report.size
-        report.precision /= classLength
-        report.recall /= classLength
-        report.fscore /= classLength
-        report.featureImportance = this.featureImportance()
-        return report
-    }
-
-    featureImportance() {
-        const r = {}
-        for (const feature of this.features) r[feature] = gain(this.data, this.target, feature)
-        return r
-    }
+    return scores
 }
